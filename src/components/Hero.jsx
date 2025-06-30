8affad5874ffc9f27dc225b3054982cfb0dadf94
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-07-01 00:26:47 +0530

    feat: enhance Hero component with GSAP animations and add content sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,47 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/all"
 
 const Hero = () => {
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {
+            type: 'chars , words',})
+
+        const ParagraphSplit = new SplitText('.subtitle', {
+            type: 'lines',})
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+        })
+
+        gsap.from(ParagraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+
+        .to('.right-leaf', { y: 200 } , 0)
+        .to('.left-leaf', { y: -200 }, 0)
+    }, [])
+
+
   return (
     <>
      <section id='hero' className='noisy'>
@@ -14,6 +56,24 @@ const Hero = () => {
         src="/images/hero-right-leaf.png" 
         alt="Right-leaf" 
         className="right-leaf" />
+
+        <div className="body">
+            <div className="content">
+                <div className="space-y-5 hidden md:block">
+                    <p>Cool. Crisp. Classic.</p>
+                    <p className="subtitle">
+                        Sip the Spirit <br /> of Summer
+                    </p>
+                </div>
+
+                <div className="view-cocktails">
+                    <p className="subtitle">
+                        "Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes – designed to delight your senses."
+                    </p>
+                    <a href="#cocktails">View Cocktails</a>
+                </div>
+            </div>
+        </div>
      </section>
     </>
   )
